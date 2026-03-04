2632cc237c084d1155b8ce225d3a40de185f6021
dawidsiwekk <dawidsiwekk@gmail.com>
2026-03-04 12:09:06 +0100

    array

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use script";
+
+const animals = [
+    {name: "Simba", species: "løve", age:"5", food: "kød"},
+    {name: "Dumbo", species: "elefant", age:"8", food: "Blade og frugt"},
+    {name: "Gerald", species: "giraf", age:"6", food: "Blade fra høje træer"}
+];
+
+ 
